d38ee5603426f0e96c1caecbbd85c276db137072
funzter <cavehive@protonmail.com>
2022-11-11 20:29:10 +0100

    add logger to plugins

--- src/main/java/de/timesnake/library/extension/util/chat/Plugin.java
+++ src/main/java/de/timesnake/library/extension/util/chat/Plugin.java
@@ -1,5 +1,5 @@
 /*
- * library-extension.main
+ * timesnake.library-extension.main
  * Copyright (C) 2022 timesnake
  *
  * This program is free software; you can redistribute it and/or
@@ -18,30 +18,45 @@
 
 package de.timesnake.library.extension.util.chat;
 
+import de.timesnake.library.basic.util.LogHelper;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Plugin {
 
-    public static final Plugin SYSTEM = new Plugin("System", "LES");
-    public static final Plugin CONSOLE = new Plugin("Console", "LEC");
-    public static final Plugin BUKKIT = new Plugin("Bukkit", "LEB");
-    public static final Plugin PROXY = new Plugin("Proxy", "LEP");
-    public static final Plugin INFO = new Plugin("Info", "LEI");
-    public static final Plugin NETWORK = new Plugin("Network", "LEN");
-    public static final Plugin TIME_COINS = new Plugin("Coins", "LET");
-    public static final Plugin PRIVATE_MESSAGES = new Plugin("Msg", "LEM");
-    public static final Plugin MAILS = new Plugin("Mails", "LEN");
+    public static final Plugin SYSTEM = new Plugin("System", "LES", LogHelper.getLogger("System", Level.INFO));
+    public static final Plugin CONSOLE = new Plugin("Console", "LEC", LogHelper.getLogger("Console", Level.INFO));
+    public static final Plugin BUKKIT = new Plugin("Bukkit", "LEB", LogHelper.getLogger("Bukkit", Level.INFO));
+    public static final Plugin PROXY = new Plugin("Proxy", "LEP", LogHelper.getLogger("Proxy", Level.INFO));
+    public static final Plugin INFO = new Plugin("Info", "LEI", LogHelper.getLogger("Info", Level.INFO));
+    public static final Plugin NETWORK = new Plugin("Network", "LEN", LogHelper.getLogger("Network", Level.INFO));
+    public static final Plugin TIME_COINS = new Plugin("Coins", "LET", LogHelper.getLogger("Coins", Level.INFO));
+    public static final Plugin PRIVATE_MESSAGES = new Plugin("Msg", "LEM", LogHelper.getLogger("Msg", Level.INFO));
+    public static final Plugin MAILS = new Plugin("Mails", "LEN", LogHelper.getLogger("Mails", Level.INFO));
+    public static final Plugin CHATS = new Plugin("Chats", "CHT", LogHelper.getLogger("Chats", Level.INFO));
+
+    // only for console
+    public static final Plugin SCOREBOARD = new Plugin("Scoreboard", "SCB", LogHelper.getLogger("Scoreboard"));
+    public static final Plugin GROUPS = new Plugin("Groups", "GRP", LogHelper.getLogger("Groups"));
+    public static final Plugin PACKETS = new Plugin("Packets", "PKT", LogHelper.getLogger("Packets"));
+    public static final Plugin WORLDS = new Plugin("Worlds", "WLD", LogHelper.getLogger("Worlds"));
+    public static final Plugin PERMISSIONS = new Plugin("Permissions", "PRM", LogHelper.getLogger("Permissions"));
+
 
     private final String name;
     private final String code;
+    private final Logger logger;
 
     private final Map<String, Code> codeByString = new HashMap<>();
     private int shortCodeCounter = 1;
 
-    protected Plugin(String name, String code) {
+    protected Plugin(String name, String code, Logger logger) {
         this.name = name;
         this.code = code;
+        this.logger = logger;
     }
 
     public String getName() {
@@ -50,6 +65,10 @@ public class Plugin {
 
     public String getCode() {
         return this.code;
+    }
+
+    public Logger getLogger() {
+        return logger;
     }
 
     /**
