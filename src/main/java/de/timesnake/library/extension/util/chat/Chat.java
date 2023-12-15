1eafd8e15308e8dd3227a8fb627169b71ab85631
funzter <cavehive@protonmail.com>
2023-12-16 00:33:00 +0100

    removed deprecated methods

--- src/main/java/de/timesnake/library/extension/util/chat/Chat.java
+++ src/main/java/de/timesnake/library/extension/util/chat/Chat.java
@@ -11,10 +11,8 @@ import de.timesnake.library.extension.util.cmd.Arguments;
 import de.timesnake.library.extension.util.cmd.Sender;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
-import net.kyori.adventure.text.flattener.ComponentFlattener;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextColor;
-import net.kyori.adventure.text.format.TextDecoration;
 import org.jetbrains.annotations.NotNull;
 
 import java.time.Duration;
@@ -62,18 +60,6 @@ public interface Chat {
 
   static Component getMessageCode(Code code, ExTextColor color) {
     return Component.text("(Code: " + code.asStringCode() + ")", color);
-  }
-
-  static Component getTimeComponent(Integer time) {
-    if (time >= 3600) {
-      return Component.text(
-          time / 3600 + "h " + (time % 3600) / 60 + "min " + time % 60 + "s");
-    }
-    if (time >= 60) {
-      return Component.text(time / 60 + "min " + time % 60 + "s");
-    } else {
-      return Component.text(time % 60 + "s");
-    }
   }
 
   static Component listToComponent(Collection<?> list) {
@@ -139,92 +125,6 @@ public interface Chat {
     return ((int) (coins * 100)) / 100f;
   }
 
-  /**
-   * @param component to parse
-   * @return Deprecated. Use {@code LegacyComponentSerializer.legacyAmpersand().serialize()}
-   */
-  @Deprecated
-  static String parseComponentToString(Component component) {
-    StringBuilder sb = new StringBuilder();
-    ComponentFlattener.basic().flatten(component, sb::append);
-    return sb.toString();
-  }
-
-  /**
-   * @param string to parse
-   * @return Deprecated. Use {@code LegacyComponentSerializer.legacyAmpersand().deserialize()}
-   */
-  @Deprecated
-  static Component parseStringToComponent(String string) {
-    TextComponent.Builder builder = Component.text();
-    if (string == null) {
-      return null;
-    }
-    for (String part : string.split("§")) {
-      if (!part.isEmpty()) {
-        if (part.length() == 1) {
-          net.kyori.adventure.text.format.NamedTextColor color = parseChatColor(
-              part.charAt(0));
-          TextDecoration decoration = parseChatColorDecoration(part.charAt(0));
-          if (color != null) {
-            builder.color(color);
-          } else if (decoration != null) {
-            builder.decorate(decoration);
-          } else {
-            builder.append(Component.text("§" + part));
-          }
-        } else {
-          part = part.substring(1);
-          net.kyori.adventure.text.format.NamedTextColor color = parseChatColor(
-              part.charAt(0));
-          TextDecoration decoration = parseChatColorDecoration(part.charAt(0));
-          if (color != null) {
-            builder.append(Component.text(part, color));
-          } else if (decoration != null) {
-            builder.append(Component.text(part).decorate(decoration));
-          } else {
-            builder.append(Component.text("§" + part));
-          }
-        }
-      }
-    }
-    return builder.build();
-  }
-
-  static net.kyori.adventure.text.format.NamedTextColor parseChatColor(char colorCode) {
-    return switch (colorCode) {
-      case '0' -> net.kyori.adventure.text.format.NamedTextColor.BLACK;
-      case '1' -> net.kyori.adventure.text.format.NamedTextColor.DARK_BLUE;
-      case '2' -> net.kyori.adventure.text.format.NamedTextColor.DARK_GREEN;
-      case '3' -> net.kyori.adventure.text.format.NamedTextColor.DARK_AQUA;
-      case '4' -> net.kyori.adventure.text.format.NamedTextColor.DARK_RED;
-      case '5' -> net.kyori.adventure.text.format.NamedTextColor.DARK_PURPLE;
-      case '6' -> net.kyori.adventure.text.format.NamedTextColor.GOLD;
-      case '7' -> net.kyori.adventure.text.format.NamedTextColor.GRAY;
-      case '8' -> net.kyori.adventure.text.format.NamedTextColor.DARK_GRAY;
-      case '9' -> net.kyori.adventure.text.format.NamedTextColor.BLUE;
-      case 'a' -> net.kyori.adventure.text.format.NamedTextColor.GREEN;
-      case 'b' -> net.kyori.adventure.text.format.NamedTextColor.AQUA;
-      case 'c' -> net.kyori.adventure.text.format.NamedTextColor.RED;
-      case 'd' -> net.kyori.adventure.text.format.NamedTextColor.LIGHT_PURPLE;
-      case 'e' -> net.kyori.adventure.text.format.NamedTextColor.YELLOW;
-      case 'f' -> net.kyori.adventure.text.format.NamedTextColor.WHITE;
-      default -> null;
-    };
-  }
-
-  static TextDecoration parseChatColorDecoration(char actionCode) {
-    return switch (actionCode) {
-      case 'k' -> TextDecoration.OBFUSCATED;
-      case 'l' -> TextDecoration.BOLD;
-      case 'm' -> TextDecoration.STRIKETHROUGH;
-      case 'n' -> TextDecoration.UNDERLINED;
-      case 'o' -> TextDecoration.ITALIC;
-      default -> null;
-    };
-  }
-
-
   @Deprecated
   static String getSplitterString() {
     return ChatColor.DARK_GRAY + "» " + ChatColor.RESET;
@@ -273,23 +173,6 @@ public interface Chat {
       sb.append(time.toMinutesPart()).append("min ");
     }
     return sb.append(time.toSecondsPart()).append("s").toString();
-  }
-
-  @Deprecated
-  static String listToString(Collection<?> list) {
-    if (list.isEmpty()) {
-      return "";
-    }
-
-    StringBuilder sb = new StringBuilder();
-
-    for (Object obj : list) {
-      sb.append(obj.toString()).append(", ");
-    }
-
-    sb.delete(sb.length() - 2, sb.length());
-
-    return sb.toString();
   }
 
   static String getLocationString(int x, int y, int z) {
